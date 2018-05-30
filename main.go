ac527a9e8ced2804642c651c1d0c15db3b411a43
Wei-Chun <chu@fitanalytics.com>
2018-05-30 16:42:46 +0200

    export state values

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
+	"os"
+	"strconv"
 )
 
 // arrayEqualsInteger checks whether all elements in an array is equal to a certain integer
@@ -45,6 +48,28 @@ func playGame(p1, p2 agent, e environment) {
 	return
 }
 
+// exportValues writes state values of the agent to a csv file
+func exportValues(vs values, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for state, value := range vs {
+		row := []string{strconv.FormatInt(state, 10), strconv.FormatFloat(value, 'g', 5, 64)}
+		err := writer.Write(row)
+		if err != nil {
+			log.Fatal("Cannot write to file", err)
+		}
+	}
+
+	return
+}
+
 // train two robots to play
 func main() {
 	p1 := agent{}
@@ -59,10 +84,12 @@ func main() {
 		playGame(p1, p2, e)
 	}
 
+	exportValues(p1.values, "p1_values.csv")
+
 	// let's see what p1 have learnt
-	valueArray := rankStateValues(p1.values)
-	for i := 0; i < 5; i++ {
-		log.Printf("best state %v, state = %v, value = %v", i, valueArray[i].state, valueArray[i].value)
-	}
+	//valueArray := rankStateValues(p1.values)
+	//for i := 0; i < 5; i++ {
+	//	log.Printf("best state %v, state = %v, value = %v", i, valueArray[i].state, valueArray[i].value)
+	//}
 
 }
