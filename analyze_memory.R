eeec0795bf40d58b2d5790659795b8ce91841ce4
Wei-Chun <chu@fitanalytics.com>
2018-06-01 13:55:54 +0200

    plot hist of values

--- analyze_memory.R
+++ analyze_memory.R
@@ -1,5 +1,27 @@
 library(tidyverse)
 
-memory1 <- read.csv('p1_values.csv', stringsAsFactors = FALSE)
-memory2 <- read.csv('p2_values.csv', stringsAsFactors = FALSE)
+memory1 <-
+  read.csv('p1_values.csv', stringsAsFactors = FALSE, header = FALSE) %>%
+  select(state = V1, value1 = V2)
+memory2 <-
+  read.csv('p2_values.csv', stringsAsFactors = FALSE, header = FALSE) %>%
+  select(state = V1, value2 = V2)
 
+memory <-
+  inner_join(memory1,
+             memory2,
+             by = "state") %>%
+  mutate(sum = value1 + value2)
+
+hist_value <-
+  ggplot(memory) +
+  geom_histogram(aes(x = value1), binwidth = 0.05, fill = 'darkred', alpha = 0.3) +
+  geom_histogram(aes(x = value2), binwidth = 0.05, fill = 'darkgreen', alpha = 0.3) +
+  labs(title = 'Histogram of state values (red = p1, green = p2)', x = 'Value', y = 'Count')
+print(hist_value)
+
+hist_value_sum <-
+  ggplot(memory) +
+  geom_histogram(aes(x = sum), binwidth = 0.002) +
+  labs(title = 'Histogram of (p1 value + p2 value)', x = 'Value', y = 'Count')
+print(hist_value_sum)
