d4a125514337bfbcc7c4bb2a2ef0b4d216d654bb
Wei-Chun <chu@fitanalytics.com>
2018-08-17 14:12:59 +0200

    update parameters

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ const nOldest = 9         // number of oldest states to remember
 const printSteps = false  // print board and plan at each step
 const alpha = 0.5         // default alpha (learning rate)
 const epsilon = 0.1       // default epsilon (probability to take random action)
-const gamma = 0.9         // default gamma (decay rate)
+const gamma = 0.5         // default gamma (decay rate)
 const initialValue = 0.0  // a (non-ending) state's initial value before iteration
 const fluctuation = 0.01  // the amplitude of fluctuation for initialValue
 const winReward = 1.0     // reward for winning the game
-const drawReward = 0.5    // reward for draw game
-const loseReward = 0.0    // reward for losing the game
+const drawReward = 0.0    // reward for draw game
+const loseReward = -1.0   // reward for losing the game
 const nPrintHistory = 100 // print value history every N points
 
 // main
